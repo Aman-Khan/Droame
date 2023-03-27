3d4b5fe2a7ce1e0fc1172825648b3b24fff9d83d
d3xter <ak722442@gmail.com>
2023-03-27 20:50:55 +0530

    register new customer page is added

--- FastAPI/frontend/js/dynamic.js
+++ FastAPI/frontend/js/dynamic.js
@@ -1,17 +1,13 @@
-$(document).ready(function() {
-    // Hide all containers initially
-    $('.container.custom-width-65').children().hide();
-  
-    // Add click event listener to list items
-    $('.list-group-item').click(function() {
-      // Get the target container
-      var target = $(this).data('target');
-      
-      // Hide all containers except the target
-      $('.container.custom-width-65').children().not(target).hide();
-      
-      // Show the target container
-      $(target).show();
-    });
-  });
-  
+// $(document).ready(function() {
+// // Set the click event for the list items
+// $('#optionBlock ul li').click(function() {
+//     // Get the target container
+//     var targetContainer = $(this).data('target');
+
+//     // Hide all containers in dynamicBlock
+//     $('#dynamicBlock > .container').hide();
+
+//     // Show the target container
+//     $(targetContainer).show();
+// });
+// });
